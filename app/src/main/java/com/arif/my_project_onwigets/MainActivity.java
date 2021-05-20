c3972c3c53a61ead3c4007bcdf8ed3bfa1331991
ARIF <arifulpub143@gmail.com>
2021-05-20 03:48:46 -0700

    master

---
+++ app/src/main/java/com/arif/my_project_onwigets/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arif.my_project_onwigets;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
